0b9dbdd85084784cf967e3757bd226311981b997
뉴하진윤 <61103309+newhajinyoon@users.noreply.github.com>
2020-05-29 14:09:47 +0900

    Update entryserver.js

--- entryserver.js
+++ entryserver.js
@@ -1,7 +1,7 @@
 entrylms.alertTextarea({
-outerHTML: '와 엔트리 서버 아시는구나 그거 겁나 재미있는서버임 (요즘엔 좀 식은거 같지만) <br><br>그런데 커뮤니티 좀 그런거 같아서 여기와서 채팅합시다!와!<br><br>앞으로는 엔트리 대신 여기서 코딩얘기 나누는거 어때요? <ㅍㅍ <br><br>그동안 감사했습니다.<br>아래 엔트리서버 버튼을 누르면 엔트리서버로 이동됩니다.<br><br><br>서버문의= newhajinyoon@gmail.com<br>기타 문의: newhajinyoon@gmail.com<br>디스코드 회원 가입: https://discord.com',
-}, '엔트리 서버 안내문', {
-positiveButtonText: '엔트리서버',
+outerHTML: 'wa! you know entryserver! that sever is so fun (in today it is little message (cry)(cry)(cry)(cry)) <br><br>i think today's community is little bad... <br><br> how about chating here! <br><br>thankyou.<br>(if you click entry server button you move to entrysever.)<br><br><br>email= newhajinyoon@gmail.com<br>second email: newhajinyoon@gmail.com<br>discord: https://discord.com<br> entrysever : https://discord.com/invite/VZ6auW8 ',
+}, 'entryserver', {
+positiveButtonText: 'entryserver',
 }).on('click', () => {
 window.location.href = 'https://discord.gg/VZ6auW8'
 })
